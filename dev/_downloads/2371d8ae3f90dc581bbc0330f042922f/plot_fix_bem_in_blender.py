5d3e08a9c53ab48714c6f427d8d3e57ee829934f
Circle CI <circle@mne.com>
2020-06-16 17:30:57 +0000

    CircleCI update of dev docs (20643).

--- dev/_downloads/2371d8ae3f90dc581bbc0330f042922f/plot_fix_bem_in_blender.py
+++ dev/_downloads/2371d8ae3f90dc581bbc0330f042922f/plot_fix_bem_in_blender.py
@@ -8,12 +8,11 @@ Here, we will see how this can be achieved by exporting the surfaces to the 3D
 modeling program `Blender <https://blender.org>`_, editing them, and
 re-importing them.
 
-This tutorial is based on https://github.com/ezemikulan/blender_freesurfer by
-Ezequiel Mikulan.
+This tutorial is based on https://github.com/ezemikulan/blender_freesurfer
 """
 
 # Authors: Marijn van Vliet <w.m.vanvliet@gmail.com>
-#          Ezequiel Mikulan <e.mikulan@gmail.com>
+#          @ezemikulan
 #
 # License: BSD (3-clause)
 
